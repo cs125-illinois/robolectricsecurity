923f28409a311986da3e905ebe2791f9d2cc8e9c
Ben Nordick <Fleex255@users.noreply.github.com>
2019-07-05 14:11:49 -0500

    Test normal functionality

--- testapp/src/main/java/edu/illinois/cs/cs125/robolectricsecurity/test/MainActivity.java
+++ testapp/src/main/java/edu/illinois/cs/cs125/robolectricsecurity/test/MainActivity.java
@@ -2,13 +2,45 @@ package edu.illinois.cs.cs125.robolectricsecurity.test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout mainLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainLayout = findViewById(R.id.mainLayout);
     }
+
+    void createUI() {
+        TextView label = new TextView(this);
+        label.setText("Not clicked yet");
+        label.setTag("label");
+        Button button = new Button(this);
+        button.setText("Click");
+        button.setOnClickListener(unused -> label.setText("Clicked"));
+        button.setTag("button");
+        mainLayout.addView(label);
+        mainLayout.addView(button);
+    }
+
+    void createImageView() {
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+        ImageView imageView = new ImageView(this);
+        imageView.setImageDrawable(new BitmapDrawable(getResources(), bitmap));
+        imageView.setTag("image");
+        mainLayout.addView(imageView);
+    }
+
 }
---
+++ testapp/src/test/java/edu/illinois/cs/cs125/robolectricsecurity/test/FunctionalityTest.kt
@@ -0,0 +1,52 @@
+package edu.illinois.cs.cs125.robolectricsecurity.test
+
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import edu.illinois.cs.cs125.robolectricsecurity.Trusted
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.Shadows
+
+@Trusted
+@RunWith(RobolectricTestRunner::class)
+class FunctionalityTest {
+
+    private lateinit var activity: MainActivity
+
+    @Before
+    fun before() {
+        ensureSecurityManagerInstalled()
+        activity = Robolectric.buildActivity(MainActivity::class.java).create().start().resume().get()
+    }
+
+    @Test
+    fun testActivitySetup() {
+        Assert.assertNotNull(activity.findViewById(R.id.mainLayout))
+    }
+
+    @Test
+    fun testViews() {
+        val layout = activity.findViewById<LinearLayout>(R.id.mainLayout)
+        Assert.assertNull(layout.findViewWithTag("label"))
+        activity.createUI()
+        val label = layout.findViewWithTag<TextView>("label")
+        Assert.assertEquals("Not clicked yet", label.text)
+        val button = layout.findViewWithTag<Button>("button")
+        button.performClick()
+        Assert.assertEquals("Clicked", label.text)
+    }
+
+    @Test
+    fun testImage() {
+        activity.createImageView()
+        val imageView = activity.findViewById<LinearLayout>(R.id.mainLayout).findViewWithTag<ImageView>("image")
+        Assert.assertEquals(R.mipmap.ic_launcher, Shadows.shadowOf(imageView.drawable).createdFromResId)
+    }
+
+}
